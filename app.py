b79a78533c91c5caad8a167aec27d2477ac9acc0
raman-webdev <chaudharyraman562@gmail.com>
2026-07-31 23:01:48 +0545

    add one new file and modify the farewell function

--- app.py
+++ app.py
@@ -3,4 +3,16 @@ def greet(name):
 
 
 def farewell(name):
+    print("Hello World!!")
     return f'Goobye, {name}!'
+
+
+def welcome(name):
+    return f'Welcome, {name}!'
+
+
+def warning(name):
+    return f'Warning to {name}.'
+
+
+
